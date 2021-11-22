9f9b4070f663923a0714bb22f1e389084f46bb7e
chloese1 <90604634+chloese1@users.noreply.github.com>
2021-11-22 09:25:15 +0000

    Add files via upload

---
+++ anagram.py
@@ -0,0 +1,33 @@
+def areAnagram(str1, str2):
+	#find lengths of both strings
+	n1 = len(str1)
+	n2 = len(str2)
+
+	# If length is not same,
+	# they cannot be anagram
+	if n1 != n2:
+		return 0
+
+	# Sort both strings
+	str1 = sorted(str1)
+	str2 = sorted(str2)
+
+	# Compare sorted strings
+	for i in range(0, n1):
+		if str1[i] != str2[i]:
+			return 0
+
+	return 1
+
+
+#test code
+str1 = input("enter an anagram")
+str2 = input("enter a second anagram")
+
+#fuction call
+if areAnagram(str1, str2):
+	print("The two strings are anagram of each other")
+else:
+	print("The two strings are not anagram of each other")
+
+
