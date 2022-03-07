031d2d38e3e2c94d4b28271cb69c541bbd332423
chloese1 <90604634+chloese1@users.noreply.github.com>
2022-03-07 09:15:07 +0000

    Add files via upload

---
+++ gannet mock.py
@@ -0,0 +1,17 @@
+while True:
+    print("""
+        1.	Enter new Contract
+        2.	Display Summary of Contracts
+        3.	Display Summary of Contracts for Selected Month
+        4.	Find and display Contract
+        0.	Exit
+        """)
+
+    ans=input ("What would you like to do? ")
+    if ans=="1":
+        input("Please enter your name")
+        input("Please enter your package")
+        input("Please enter your data bundle")
+        input("Please enter your reference")
+        input("How long do you want this contract")
+        input("Would you like international calls")
